843dbdcf36d2405ed94fe97b264473d66050be41
Sergii <sergiinari@gmail.com>
2022-10-17 17:39:40 +0300

    first version

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function firstTask() {
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+
+    return result;
+}
+
+console.log(firstTask());
